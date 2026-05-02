09a71b30f28f37ac3cd4e856f1010a4d15df0679
thiendaoxuan <thiendaoxuan1@gmail.com>
2026-05-02 09:03:13 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'geodetic-v1'
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll(['/geodetic-tools/'])
+    )
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached ||
+      fetch(e.request).then((res) => {
+        if (res.ok) {
+          const clone = res.clone()
+          caches.open(CACHE).then((cache) => cache.put(e.request, clone))
+        }
+        return res
+      })
+    )
+  )
+})
